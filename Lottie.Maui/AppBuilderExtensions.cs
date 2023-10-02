0b54768815be64e7e38d0993503a870dbc5ac5b1
Zhen Zhi <zhenzhi.lee@anacle.com>
2023-10-02 10:25:57 +0800

    LZZ: Add UseLottie extension method

---
+++ Lottie.Maui/AppBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace Lottie.Forms;
+
+public static class AppBuilderExtensions
+{
+    public static MauiAppBuilder UseLottie(this MauiAppBuilder builder)
+    {
+        builder
+            .UseMauiCompatibility()
+            .ConfigureMauiHandlers(handlers =>
+            {
+#if ANDROID
+                    handlers.AddCompatibilityRenderer(typeof(Lottie.Forms.AnimationView), typeof(Lottie.Forms.Platforms.Android.AnimationViewRenderer));
+#endif
+#if IOS
+                handlers.AddCompatibilityRenderer(typeof(Lottie.Forms.AnimationView),
+                    typeof(Lottie.Forms.Platforms.Ios.AnimationViewRenderer));
+#endif
+            });
+        return builder;
+    }
+}
